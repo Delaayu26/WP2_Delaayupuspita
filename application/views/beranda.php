2f3fb63229e641fd736fc7a93867cc59715979eb
delaayu26 <delaayu2612@gmail.com>
2021-10-29 09:25:35 +0700

    menambahkan folder tugas1_12201179

---
+++ application/views/beranda.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Hai nama saya adalah Dela Ayu Puspita</title>
+
+	
+</head>
+<body>
+
+<div id="container">
+	<h1>Hai nama saya adalah Dela Ayu Puspita</h1>
+</body>
+</html>
